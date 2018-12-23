995475e8d00b4407dfb74d5c57f0ba30e22dc8ef
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2018-12-23 15:40:22 -0400

    cambios del 23-12-2018

---
+++ brackets/plug-in.sh
@@ -0,0 +1,31 @@
+#temas
+
+Varz Dark
+
+
+# plug in
+
+#no usados
+TypeScript
+Special Html Characters
+Autoprefixer
+ESLint
+quickJS
+Brackets Icons
+
+#usados
+Beautify
+Brackets Color Palette
+Brackets Css Color Preview
+Brackets File Icons
+Brackets Matcher
+Brackets Outline List
+Brackets Snippets (by edc)
+EditorConfig
+Emmet
+jQuinter
+Lorem Ipsum
+PHP Completion (AutoComplete)
+RCH Bootstrap 4 Snippets
+
+
