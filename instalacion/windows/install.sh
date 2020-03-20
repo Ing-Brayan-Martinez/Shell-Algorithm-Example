b13a8a94605077eb36dd46425f45a32c32250c9a
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2020-03-20 17:30:11 -0400

    agregar samba y ssh

---
+++ instalacion/windows/install.sh
@@ -0,0 +1,87 @@
+#CLI
+choco install curl -y
+choco install wget -y
+choco install cygwin -y
+choco install sudo -y
+
+#javascript
+choco install vscode -y 
+choco install brackets -y
+choco install nodejs-lts -y 
+choco install webpacktaskrunner -y 
+choco install electron -y
+choco install sass -y
+
+#devops
+choco install kubernetes-cli -y
+choco install docker-compose -y
+choco install docker-kitematic -y
+choco install faas-cli -y
+choco install manifest-tool -y
+choco install skaffold -y
+choco install draft -y
+choco install terraform -y
+choco install jenkins -y
+choco install mobaxterm -y
+choco install elasticsearch -y
+
+#php
+choco install apache-httpd -y
+choco install php -y
+choco install composer -y
+choco install mysql -y
+
+#control de versiones
+choco install tortoisesvn -y
+choco install svn -y
+choco install git -y
+choco install winmerge -y
+choco install gitkraken -y
+
+#java
+choco install nexus-repository -y
+choco install maven -y
+choco install gradle -y
+choco install ant -y
+choco install groovy -y
+choco install springtoolsuite -y
+choco install postman -y
+choco install androidstudio -y
+choco install postgresql -y
+choco install mongodb -y
+choco install rabbitmq -y
+choco install dbeaver -y
+
+#python
+choco install anaconda3 -y
+choco install python3 -y
+
+#.NET
+choco install visualstudio2017-installer -y
+choco install visualstudio2017community -y
+choco install sql-server-management-studio
+choco install dotnetcore-sdk -y
+choco install sql-server-expres -y
+choco install aspnetcore-runtimepackagestore -y
+
+#otros lenguajes
+choco install rust -y
+choco install mingw -y
+
+#otros
+choco install lightshot -y
+choco install rufus -y
+choco install peazip -y
+choco install clover -y
+choco install bleachbit -y
+choco install keepass.install -y
+choco install chromium -y
+choco install qbittorrent -y
+choco install office365proplus -y
+choco install mkvtoolnix -y
+
+
+#uninstall
+
+
+
