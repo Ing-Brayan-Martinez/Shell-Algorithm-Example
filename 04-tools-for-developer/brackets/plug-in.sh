e34f0340bf9c489c8eb981008e5433999f868d2f
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2020-10-12 10:52:13 -0400

    Update plug-in.sh

--- 04-tools-for-developer/brackets/plug-in.sh
+++ 04-tools-for-developer/brackets/plug-in.sh
@@ -10,25 +10,29 @@ Beautify
 Brackets Color Palette
 Brackets Css Color Preview
 Brackets Ungit
-Brackets Icons
 Brackets Matcher
 Brackets Outline List
 Brackets Snippets (by edc)
+Brackets Markdown Preview
 EditorConfig
 Emmet
 jQuinter
 Lorem Ipsum
 QuickDocsJS
 quickJS
-RCH Bootstrap 4 Snippets
-Vue Snippets
+Smart JSDoc Comment
+TypeScript
 Open project in terminal
+Math Visualization
+Multi-Line Middle Click
+NodeJS integration
 
 #plug in no usados
-TypeScript
+
 Special Html Characters
 Autoprefixer
 ESLint
 Brackets Thymeleaf Code Hint
-
+RCH Bootstrap 4 Snippets
+Vue Snippets
 
